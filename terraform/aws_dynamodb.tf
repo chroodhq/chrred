23f3f96fb83c199647d16fa9cd9993d6aa540271
Colin Tessarzick <colintessarzick@hotmail.com>
2023-04-23 16:28:28 +0200

    refactor(LINK-2): change db table name

--- terraform/aws_dynamodb.tf
+++ terraform/aws_dynamodb.tf
@@ -1,5 +1,5 @@
 resource "aws_dynamodb_table" "matching_table" {
-  name         = "${local.resource_prefix}-matching-table"
+  name         = "${local.resource_prefix}-link-matching"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "url_key"
 
