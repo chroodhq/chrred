3d8f1e204d02790c953537af25ae0a448663ed33
Colin Tessarzick <colintessarzick@hotmail.com>
2023-04-23 14:38:56 +0200

    feat(LINK-2): prepare creating urls

--- src/interfaces/lambda_function.py
+++ src/interfaces/lambda_function.py
@@ -1,8 +1,10 @@
+import json
+import validators
+
 from aws_lambda_powertools import Logger, Tracer
 from aws_lambda_powertools.event_handler import APIGatewayRestResolver, Response, CORSConfig
 from aws_lambda_powertools.logging import correlation_paths
 from aws_lambda_powertools.utilities.typing import LambdaContext
-import validators
 
 from src.domain import models
 
@@ -22,12 +24,16 @@ def welcome() -> Response:
     )
 
 
-@app.post("/<url>")
+@app.post("/url")
 @tracer.capture_method
-def create_url(url: models.URLBase) -> Response:
+def create_url() -> Response:
+    body = json.loads(str(app.current_event.body))
+    url: models.URLBase = models.URLBase(**body)
+
     if not validators.url(url.target_url):
         return Response(body="Bad Request! Your provided URL is not valid.", status_code=400)
-    return Response(body=f"TODO: Create database entry for: {url.target_url}", status_code=200)
+    else:
+        return Response(body=f"TODO: Create database entry for: {url.target_url}", status_code=200)
 
 
 @logger.inject_lambda_context(correlation_id_path=correlation_paths.API_GATEWAY_REST)
