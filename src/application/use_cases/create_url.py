a6260758083535788bda41d8cc4876fcefac9139
Colin Tessarzick <colintessarzick@hotmail.com>
2023-04-23 15:21:20 +0200

    feat(LINK-2): store url in database

--- src/application/use_cases/create_url.py
+++ src/application/use_cases/create_url.py
@@ -3,19 +3,26 @@ import string
 import uuid
 
 from src.domain.models import URL
-from src.domain.schemas import URLBase
+from src.domain.schemas import URLBase, URLInfo
+from src.infrastructure.repositories.dynamodb_matching_table import DynamoDBMatchingTableRepository
 
 
 class CreateURLUseCase:
     def __init__(self) -> None:
-        pass
+        self.repository = DynamoDBMatchingTableRepository()
 
-    def run(self, url: URLBase) -> URL:
-        return URL(
-            id=str(uuid.uuid4()),
-            key="".join(random.choices(string.ascii_letters + string.digits, k=5)),
-            secret_key="".join(random.choices(string.ascii_letters + string.digits, k=12)),
-            target_url=url.target_url,
-            is_active=True,
-            clicks=0,
-        )
+    def run(self, url_base: URLBase) -> URLInfo:
+        try:
+            url = URL(
+                id=str(uuid.uuid4()),
+                key="".join(random.choices(string.ascii_letters + string.digits, k=5)),
+                secret_key="".join(random.choices(string.ascii_letters + string.digits, k=12)),
+                target_url=url_base.target_url,
+                is_active=True,
+                clicks=0,
+            )
+            self.repository.create_item(url)
+        except Exception as e:
+            raise e
+        else:
+            info = URLInfo(**url.__dict__)
---
+++ src/infrastructure/repositories/dynamodb_matching_table.py
@@ -0,0 +1,43 @@
+import boto3
+
+from src.configuration import config
+from src.domain.models import URL
+
+
+class DynamoDBMatchingTableRepository:
+    def __init__(self) -> None:
+        self.client = boto3.client("dynamodb")
+        self.table_name = config.database_table_name
+
+    def create_item(self, url: URL) -> dict:
+        try:
+            response = self.client.put_item(
+                TableName=self.table_name,
+                Item=self.convert_url_to_dynamodb_item(url),
+            )
+        except Exception as e:
+            raise e
+        else:
+            return response
+
+    @staticmethod
+    def convert_url_to_dynamodb_item(url: URL) -> dict:
+        return {
+            "id": {"S": url.id},
+            "key": {"S": url.key},
+            "secret_key": {"S": url.secret_key},
+            "target_url": {"S": url.target_url},
+            "is_active": {"BOOL": url.is_active},
+            "clicks": {"N": str(url.clicks)},
+        }
+
+    @staticmethod
+    def convert_dynamodb_item_to_url(item: dict) -> URL:
+        return URL(
+            id=item["id"]["S"],
+            key=item["key"]["S"],
+            secret_key=item["secret_key"]["S"],
+            target_url=item["target_url"]["S"],
+            is_active=item["is_active"]["BOOL"],
+            clicks=int(item["clicks"]["N"]),
+        )
