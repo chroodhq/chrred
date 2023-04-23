de8abdf4aea8e3ae79304e8657279aef38d95bb4
Colin Tessarzick <colintessarzick@hotmail.com>
2023-04-23 14:58:10 +0200

    feat(LINK-2): prepare creating urls

--- src/application/use_cases/create_url.py
+++ src/application/use_cases/create_url.py
@@ -13,8 +13,8 @@ class CreateURLUseCase:
     def run(self, url: URLBase) -> URL:
         return URL(
             id=uuid.uuid4(),
-            key=''.join(random.choices(string.ascii_letters + string.digits, k=5)),
-            secret_key=''.join(random.choices(string.ascii_letters + string.digits, k=12)),
+            key="".join(random.choices(string.ascii_letters + string.digits, k=5)),
+            secret_key="".join(random.choices(string.ascii_letters + string.digits, k=12)),
             target_url=url.target_url,
             is_active=True,
             clicks=0,
