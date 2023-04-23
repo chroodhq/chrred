67d60b3830e644cf22e37cfb39bc0115054a02a9
Colin Tessarzick <colintessarzick@hotmail.com>
2023-04-23 16:16:27 +0200

    refactor(LINK-2): change db hash key

--- terraform/aws_dynamodb.tf
+++ terraform/aws_dynamodb.tf
@@ -1,10 +1,10 @@
 resource "aws_dynamodb_table" "matching_table" {
   name         = "${local.resource_prefix}-matching-table"
   billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "id"
+  hash_key     = "url_key"
 
   attribute {
-    name = "id"
+    name = "url_key"
     type = "S"
   }
   deletion_protection_enabled = true
