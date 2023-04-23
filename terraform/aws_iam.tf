1c561a42f4dccbf1f2a8e94580783f0c665ccdb6
Colin Tessarzick <colintessarzick@hotmail.com>
2023-04-23 16:55:50 +0200

    fix(LINK-2): add missing permission to lambda function

--- terraform/aws_iam.tf
+++ terraform/aws_iam.tf
@@ -28,6 +28,7 @@ data "aws_iam_policy_document" "lambda_permissions" {
     actions = [
       "dynamodb:PutItem",
       "dynamodb:Query",
+      "dynamodb:UpdateItem"
     ]
     resources = ["*"]
   }
